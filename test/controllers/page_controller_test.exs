44bb61f651f84250106395faf715175dca3c00f6
Shiyu Du <dsy1998@live.com>
2017-04-27 15:16:10 -0400

    Add controller tests.

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -1,8 +1,42 @@
-# defmodule Hnet.PageControllerTest do
-#   use Hnet.ConnCase
+defmodule Hnet.PageControllerTest do
+  use Hnet.ConnCase
 
-#   test "GET /", %{conn: conn} do
-#     conn = get conn, "/"
-#     assert html_response(conn, 200) =~ "Welcome to Phoenix!"
-#   end
-# end
+  import Hnet.DefaultModels
+
+  test "home page with anonymous user", %{conn: conn} do
+    conn = get conn, page_path(conn, :index)
+    assert html_response(conn, 200) =~ "Welcome to HealthNet!"
+  end
+
+  test "homepage with patient user", %{conn: conn} do
+    create_default_hospital()
+    create_default_doctor()
+    create_default_patient()
+
+    credentials = %{
+      "username" => "hound",
+      "password" => "phoenix"
+    }
+
+    conn = conn
+    |> post(auth_path(conn, :login), login_credentials: credentials)
+    |> get(page_path(conn, :index))
+
+    assert redirected_to(conn) =~ page_path(conn, :patient)
+  end
+
+  test "homepage with administrator user", %{conn: conn} do
+    create_default_hospital()
+    user_id = create_default_administrator().id
+
+    conn = conn
+    |> bypass_through(Hnet.Router, [:browser])
+    |> get("/")
+    |> put_session(:current_user_id, user_id)
+    |> send_resp(:ok, "")
+    |> recycle
+    |> get(page_path(conn, :index))
+
+    assert redirected_to(conn) =~ page_path(conn, :administrator)
+  end
+end
--- test/support/default_models.ex
+++ test/support/default_models.ex
@@ -6,6 +6,7 @@ defmodule Hnet.DefaultModels do
   
   alias Hnet.Hospital
   alias Hnet.Account.User
+  alias Hnet.Account.Doctor
 
   def create_default_hospital do
     record = %Hospital{name: "Baelor's Sept", location: "King's Landing"}
@@ -17,9 +18,35 @@ defmodule Hnet.DefaultModels do
     hospital_id = List.first(Repo.all(query))
     %User{address: "Baelor's Sept", email: "high.sparrow@mail.com", first_name: "高",
           last_name: "麻雀", gender: "male", phone: "1230984576", username: "hsparrow",
-          password_hash: "agw8w3ohwgf8hW49", account_type: :doctor}
+          account_type: :doctor, password: "phoenix",
+          password_hash: "$2b$12$EoAdWD1LeOKTdB67l5KLBO1hM9dkTy.dsSue8Ss9o/spzZs4g7Sr2"}
     |> change
     |> put_assoc(:doctor, %{hospital_id: hospital_id})
+    |> Repo.insert!
+  end
+
+  def create_default_administrator do
+    query = from h in Hospital, select: h.id, limit: 1
+    hospital_id = List.first(Repo.all(query))
+    %User{address: "Casterly Rock", email: "tyrion.lannister@mail.com", first_name: "Tyrion",
+          last_name: "Lannister", gender: "male", phone: "1029384756", username: "tyrlan",
+          account_type: :administrator, password: "phoenix",
+          password_hash: "$2b$12$EoAdWD1LeOKTdB67l5KLBO1hM9dkTy.dsSue8Ss9o/spzZs4g7Sr2"}
+    |> change
+    |> put_assoc(:administrator, %{hospital_id: hospital_id})
+    |> Repo.insert!
+  end
+
+  def create_default_patient do
+    query = from d in Doctor, select: d.id, limit: 1
+    doctor_id = List.first(Repo.all(query))
+
+    %User{address: "Somewhere", email: "the.hound@mail.com", first_name: "Sandor",
+          last_name: "Clegane", gender: "male", phone: "1230984576", username: "hound",
+          account_type: :patient, password: "phoenix",
+          password_hash: "$2b$12$EoAdWD1LeOKTdB67l5KLBO1hM9dkTy.dsSue8Ss9o/spzZs4g7Sr2"}
+    |> change
+    |> put_assoc(:patient, %{proof_of_insurance: "proof", primary_doctor_id: doctor_id})
     |> Repo.insert!
   end
 end
